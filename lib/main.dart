db4998e026a8a01102e441d80c1b95875ef9ecf9
chigakucat <35723931+chigakucat@users.noreply.github.com>
2021-02-01 20:04:11 +0900

    fix fonts

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'SourceHanSansJP-Normal',
       ),
       home: HomePage(),
     );
