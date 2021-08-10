71cefbd3f65af25bd4a3ad4c1d72ed2074acbcd6
supermouse732e73 <supermouse732e73+supermouse732e73@noreply.github.com>
2021-08-11 08:28:26 +0900

    main.dartをテスト用に書き換えていたのをもどす

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ufoff/home_page/home_page.dart';
-import 'package:ufoff/notify_engine/notify_engine.dart' as NotifyEng;
 
 void main() {
   runApp(MyApp());
@@ -10,11 +9,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var n=NotifyEng.ufoNotify();
-    n.initialize();
     Firebase.initializeApp();
     return MaterialApp(
-      title: 'UchikoshiFesOnline',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
